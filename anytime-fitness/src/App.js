7405d8d1660561059dfd804589bf872b5928a8fe
duointherain <duointherain@gmail.com>
2020-11-16 14:56:40 -0800

    start of the marketing pages

--- anytime-fitness/src/App.js
+++ anytime-fitness/src/App.js
@@ -1,21 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './components/App.css';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import SignUp from './components/SignUp'
 import Login from './components/Login'
+import Buttons from './components/Buttons'
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/SignUp" component={SignUp} />
-          <Route path="/Login" component={Login} />
-        </Switch>
+    <div className='App'>
+      <div className="rowC">
+      <a href="#"><Buttons name='Home' /></a>
+<a href="#"><Buttons name='About' /></a>
+    <a href="#"><Buttons name='Calculators' /></a>
+    <a href="#"><Buttons name='Login' /></a>
+    <a href="#"><Buttons name='Register' /></a>
+    </div>
+    
+   <h1>Anywhere Fitness Coming Soon!</h1>
+    
+    </div>
+
+
+    
+  //   // <Router>
+  //   //   <div className="App">
+  //   //     <Switch>
+  //   //       <Route path="/SignUp" component={SignUp} />
+  //   //       <Route path="/Login" component={Login} />
+  //   //     </Switch>
         
-      </div>
-    </Router>
+  //   //   </div>
+  //   // </Router>
   );
 }
 
---
+++ anytime-fitness/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function Team(props){
+    return(
+    
+    <div className="box2"><p>Team info here {props.name} </p></div>
+    
+    );
+
+}
+
+export default Team;

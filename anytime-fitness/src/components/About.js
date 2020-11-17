b80feef812272422bb5c5cab9b3a785141045f35
duointherain <duointherain@gmail.com>
2020-11-16 21:15:15 -0800

    about page

--- anytime-fitness/src/components/About.js
+++ anytime-fitness/src/components/About.js
@@ -1,14 +1,46 @@
 
 import Navbar from './Navbar'
+import imageMax from '../images/max500.png'
+import imagePH from '../images/placeholder.png'
 
 function About(){
     return(
     
     <div>
-        <Navbar/>
-        <h1>This is our Team!</h1>
+        <div className='rowC'><Navbar/></div>
+        
+        <div className='main'>
+        <div>
+        <h1 className='topMargin'>This is our Team!</h1>
+        <p>We are a students at Lambda School, building cool sites.</p>
         </div>
-    
+        </div> 
+        {/* main */}
+        
+
+        <div className='teamPhotoBar'>
+            <div><img src={imageMax} className="teamPhoto" alt="Image of a blue haired web developer named Max" />
+            <p>Max is devoted to UI/UX.</p>
+            </div>
+
+            <div>
+            <img src={imagePH} className="teamPhoto" alt="placeholder" />
+            <p>coming soon</p>
+
+            </div>
+            <div>
+            <img src={imagePH} className="teamPhoto" alt="Placeholder" />
+            <p>coming soon</p>
+
+            </div>
+        
+
+        </div>
+
+        
+         
+    </div> 
+    // last
     );
 
 }

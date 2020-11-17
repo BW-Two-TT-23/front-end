3e3eba5977f9257ab8cecbf025983d0de5499fc4
duointherain <duointherain@gmail.com>
2020-11-16 23:22:52 -0800

    last update for the day

--- anytime-fitness/src/components/Tools.js
+++ anytime-fitness/src/components/Tools.js
@@ -1,9 +1,12 @@
 
 import Navbar from './Navbar'
 import numbers from '../images/numbers.png'
+import Bmi from './Bmi'
 
 function Tools(){
     return(
+
+       
     
     <div>
         <div className='rowC'>
@@ -14,23 +17,27 @@ function Tools(){
         </div>
         <div className='rowC'>
             <img src={numbers} className="formImage" alt="a heart and the words your numbers" />
-        <form>
+        <form >
             <h2>Calculate your BMI</h2>
             
             <div>
                 
             <input type='text'
-            placeholder="weight"></input>
+            placeholder="weight"
+            name="weightBMI"
+            ></input>
             
             </div>
             <div>
             
             <input type='text'
-            placeholder="height">
+            placeholder="height"
+            name="heightBMI"
+            >
         
             </input>
             <div>
-                <button>Calculate</button>
+                <button onClick={Bmi}>Calculate</button>
             </div>
             <h2>Your BMI is: </h2>
             </div>
